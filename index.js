0511c5356caf8599f1d75b4c3929e7bca4a0ea3e
Sean Parmelee <sparmelee@homeaway.com>
2018-12-14 11:13:25 -0600

    disable logging on all other events

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var SeedReporter = function(baseReporterDecorator) {
   baseReporterDecorator(this);
 
+  var noop = function() {};
+
+  this.onBrowserError = noop;
+  this.onBrowserLog = noop;
+  this.onSpecComplete = noop;
+  this.onRunComplete = noop;
+
   this.onBrowserComplete = function(_browser, result) {
     if (result.order && result.order.random && result.order.seed) {
       this.write(`Jasmine Seed: Randomized with seed ${result.order.seed}. Set config option 'karma.client.jasmine.random' to re-run with same order.\n`);
