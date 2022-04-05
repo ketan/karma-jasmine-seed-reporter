d3c6cbef90b31c4eb04dfeb3a2df6794cf781b1a
Christopher Carman <carmanchris31@gmail.com>
2022-04-05 07:12:54 -0700

    Update message to reference correct config option

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var SeedReporter = function(baseReporterDecorator) {
 
   this.onBrowserComplete = function(_browser, result) {
     if (result.order && result.order.random && result.order.seed) {
-      this.write(`Jasmine Seed: Randomized with seed ${result.order.seed}. Set config option 'karma.client.jasmine.random' to re-run with same order.\n`);
+      this.write(`Jasmine Seed: Randomized with seed ${result.order.seed}. Set config option 'karma.client.jasmine.seed' to re-run with same order.\n`);
     }
   };
 };
